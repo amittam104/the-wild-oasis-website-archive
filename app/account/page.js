afe87b4daa30268c9e88516c41256da65d40777d
Amit Tambulkar <amittambulkar104@gmail.com>
2024-08-18 11:41:35 +0530

    chore: import css, set metadatam, update header and nav

--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Your Account",
+};
+
 export default function Page() {
   return <div>Your account</div>;
 }
--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,34 @@
+import Header from "./_components/Header";
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
+import "@/app/_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s - The Wild Oasis",
+    default: "Welcome to The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian dolomites, surrounded by beutiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
